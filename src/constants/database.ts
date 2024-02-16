50737eafe02a5059974ed10bc900371ebfbe1fdd
sebassil12 <anddysilva15@gmail.com>
2024-02-15 21:44:33 -0500

    Add more elements to database

--- src/constants/database.ts
+++ src/constants/database.ts
@@ -2,15 +2,15 @@ export const characters =[
     {
         id:1,
         image: ["/images/Aya_Huma/1.png","/images/Aya_Huma/2.png","/images/Aya_Huma/3.png"],
-        price: "$??",
-        size:"??",
+        price: " ",
+        size:"A tu elección",
         name:"Aya Huma" 
     },
     {
         id:2,
         image:["/images/Coraline/1.png","/images/Coraline/2.png", "/images/Coraline/3.png"],
         price: "$14,99",
-        size:"??",
+        size:"15cm",
         name:"Coraline"
         
     },
@@ -18,7 +18,7 @@ export const characters =[
         id:3,
         image:["/images/CatBus/1.png","/images/CatBus/1.png", "/images/CatBus/1.png"],
         price: "$29,99",
-        size:"??",
+        size:"15cm",
         name:"Catbus"
     },
     {
@@ -27,6 +27,41 @@ export const characters =[
         price:"$8,00",
         size:"15cm",
         name:"Luffy"
+    },
+    {
+        id:5,
+        image:["/images/Batman/1.png", "/images/Batman/2.png", "/images/Batman/3.png"],
+        price: "$12,00",
+        size:"15cm",
+        name:"Batman"
+    },
+    {
+        id:6, 
+        image:["/images/Bender/1.png", "images/Bender/2.png", "images/Bender/3.png"],
+        price:"$34,99",
+        size:"50cm",
+        name:"Bender"
+    },
+    {
+        id:7,
+        image:["/images/CR7/1.png", "images/CR7/2.png"],
+        price:"$15,00",
+        size:"15cm",
+        name:"CR7"
+    },
+    {
+        id:8, 
+        image:["/images/FinnElHumano/1.png", "images/FinnElHumano/2.png", "images/FinnElHumano/3.png"],
+        price:"$15,00",
+        size:"15cm",
+        name:"Finn el Humano"
+    },
+    {
+        id:9, 
+        image:["/images/Spiderman/1.png", "images/Spiderman/2.png"],
+        price: "$14,99", 
+        size:"18cm", 
+        name:"Spiderman"
     }
 ]
 
@@ -47,11 +82,25 @@ export const bouquets = [
     },
     {
         id:3,
-        image:["/images/RamoGirasoles/1.png"],
+        image:["/images/RamoGirasoles/1.png", "/images/RamoGirasoles/2.png", "/images/RamoGirasoles/3.png"],
         price:"$12,99",
         size:"??",
         name:"Ramo de Girasoles"
     },
+    {
+        id:4,
+        image:["/images/RamoRosas/1.png"],
+        price: " ",
+        size: "Tú eliges",
+        name:"Ramo de rosas", 
+    },
+    {
+        id:5, 
+        image:["/images/RamoTulipanes/1.png","/images/RamoTulipanes/2.png", "/images/RamoTulipanes/3.png"],
+        price:" ",
+        size:"Tú eliges",
+        name: "Ramo de Tulipanes",
+    }
 
 
 ]
@@ -65,7 +114,7 @@ export const flowers = [{
 },
 {
     id:2,
-    image:["/images/Tulipanes/1.png"],
+    image:["/images/Tulipanes/1.png", "/images/Tulipanes/2.png", "/images/Tulipanes/3.png"],
     price:"$2,25",
     size:"??",
     name:"Tulipanes"
@@ -78,5 +127,19 @@ export const flowers = [{
     name:"Rosas"
 
 
+},
+{
+    id:4, 
+    image:["/images/Rosa_Morada/1.png"],
+    price: " ",
+    size: "A tu gusto",
+    name:"Rosa Morada"
+},
+{
+    id:5, 
+    image:["/images/Tulipanes_Paquete/1.png"],
+    price: " ",
+    size: "A tu gusto", 
+    name:"Paquete de tulipanes"
 }
 ]
