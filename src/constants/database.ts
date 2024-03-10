20130f3c7f6865e57ca163e743af1c834f13041f
sebassil12 <anddysilva15@gmail.com>
2024-03-10 19:37:44 -0400

    Add products and resize screen

--- src/constants/database.ts
+++ src/constants/database.ts
@@ -3,14 +3,14 @@ export const characters =[
         id:1,
         image: ["/images/Aya_Huma/1.webp","/images/Aya_Huma/2.webp","/images/Aya_Huma/3.webp"],
         price: " ",
-        size:"A tu elección",
+        size:"22cm",
         name:"Aya Huma" 
     },
     {
         id:2,
         image:["/images/Coraline/1.png","/images/Coraline/2.png", "/images/Coraline/3.png"],
         price: "$14,99",
-        size:"15cm",
+        size:"20cm",
         name:"Coraline"
         
     },
@@ -18,14 +18,14 @@ export const characters =[
         id:3,
         image:["/images/CatBus/1.png","/images/CatBus/1.png", "/images/CatBus/1.png"],
         price: "$29,99",
-        size:"15cm",
+        size:"30cmx30cm",
         name:"Catbus"
     },
     {
         id:4,
         image:["/images/Luffy/1.webp","/images/Luffy/2.webp","/images/Luffy/3.webp" ],
         price:"$8,00",
-        size:"15cm",
+        size:"12cm-15cm",
         name:"Luffy"
     },
     {
@@ -39,66 +39,81 @@ export const characters =[
         id:6, 
         image:["/images/Bender/1.png", "images/Bender/2.png", "images/Bender/3.png"],
         price:"$34,99",
-        size:"50cm",
+        size:"30cm",
         name:"Bender"
     },
     {
         id:7,
         image:["/images/CR7/1.png", "images/CR7/2.png"],
         price:"$15,00",
-        size:"15cm",
+        size:"18cm",
         name:"CR7"
     },
     {
         id:8, 
         image:["/images/FinnElHumano/1.png", "images/FinnElHumano/2.png", "images/FinnElHumano/3.png"],
         price:"$15,00",
-        size:"15cm",
+        size:"23cm",
         name:"Finn el Humano"
     },
     {
         id:9, 
         image:["/images/Spiderman/1.webp", "images/Spiderman/2.webp"],
         price: "$14,99", 
-        size:"18cm", 
+        size:"20cm", 
         name:"Spiderman"
+    },
+    {
+        id:10,
+        image:["/images/Jake_el_perro/1.webp"],
+        price:"$14,99",
+        size:"20cm-22cm",
+        name:"Jake el perro"
+    },
+    {
+        id:11,
+        image:["/images/Pochita/1.webp"],
+        price:"$19,99",
+        size:"22cm",
+        name:"Pochita"
+    },
+    {
+        id:12,
+        image:["/images/Stich/1.webp"],
+        price:"$29,99",
+        size:"25cm-28cm",
+        name:"Stich"
+    },
+    {
+        id:13,
+        image:["/images/Totoro/1.webp"],
+        size:"12cm-15cm",
+        price:"$7,00",
+        name:"Totoro"
+
     }
 ]
 
 export const bouquets = [
     {
         id:1,
-        image:["/images/RamoGirasolTulipan/1.png"],
-        price:"",
-        size:"",
-        name:"Ramitos personalizados"
-    },
-    {
-        id:2,
-        image:["/images/RamoTulipan/1.png"],
-        price:"",
-        size:"",
-        name:"Ramo de tulipanes"
-    },
-    {
-        id:3,
         image:["/images/RamoGirasoles/1.png", "/images/RamoGirasoles/2.png", "/images/RamoGirasoles/3.png"],
         price:"$12,99",
         size:"",
         name:"Ramo de Girasoles"
     },
     {
-        id:4,
-        image:["/images/RamoRosas/1.png"],
+        id:2,
+        image:["/images/RamoRosas/1.png","/images/RamoGirasolTulipan/1.png"],
         price: " ",
-        size: "Tú eliges",
+        size: "",
         name:"Ramo de rosas", 
     },
     {
-        id:5, 
-        image:["/images/RamoTulipanes/1.png","/images/RamoTulipanes/2.png", "/images/RamoTulipanes/3.png"],
+        id:3, 
+        image:["/images/RamoTulipanes/1.png","/images/RamoTulipanes/2.png", "/images/RamoTulipanes/3.png","/images/RamoTulipan/1.png"],
         price:" ",
-        size:"Tú eliges",
+        size:"",
         name: "Ramo de Tulipanes",
     }
 
@@ -114,32 +129,30 @@ export const flowers = [{
 },
 {
     id:2,
-    image:["/images/Tulipanes/1.png", "/images/Tulipanes/2.png", "/images/Tulipanes/3.png"],
+    image:["/images/Tulipanes/1.png", "/images/Tulipanes/2.png", "/images/Tulipanes/3.png", "/images/Tulipanes_Paquete/1.png"],
     price:"$2,25",
     size:"",
     name:"Tulipanes"
 },
 {
     id:3,
-    image:["/images/Rosas/1.png","/images/Rosas/2.png","/images/Rosas/3.png"],
+    image:["/images/Rosas/1.png","/images/Rosas/2.png","/images/Rosas/3.png", "/images/Rosa_Morada/1.png"],
     price:"$3,50",
     size:"",
     name:"Rosas"
-
-
 },
 {
-    id:4, 
-    image:["/images/Rosa_Morada/1.png"],
-    price: " ",
-    size: "A tu gusto",
-    name:"Rosa Morada"
+    id:4,
+    image:["/images/sunflower.png"],
+    price:"$3,50 c/u",
+    size:"",
+    name:"Girasoles"
 },
 {
-    id:5, 
-    image:["/images/Tulipanes_Paquete/1.png"],
-    price: " ",
-    size: "A tu gusto", 
-    name:"Paquete de tulipanes"
+    id:5,
+    image:["/images/Christmas_Flower/1.webp", "/images/Christmas_Flower/2.webp"],
+    price:"$20,00 (3 incluídas)",
+    size:"",
+    name:"Flor de Navidad"
 }
 ]
